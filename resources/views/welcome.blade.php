5943668e7b3f4e9d2ad32beb241be91b77bf39bd
Bojan Jovanoski <jovanoskibojan.w@gmail.com>
2024-10-20 13:53:33 +0200

    deleted dump

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    @dump(Auth::user())
     <title>Laravel</title>
 
     <!-- Fonts -->
